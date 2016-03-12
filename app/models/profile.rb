4e0f5ba37f5a51387015685f43af9912e2f59fb0
Adrian Agius <agiuslaw@gmail.com>
2016-03-12 14:25:31 +1100

    added a card

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,9 @@ class Profile < ActiveRecord::Base
 
   validates :first_name, :last_name, :dob, presence: true
 
+  mount_uploader :profile_pic, ProfilePicUploader
+
+  def full_name
+    [first_name, last_name].compact.join(' ')
+  end
 end
